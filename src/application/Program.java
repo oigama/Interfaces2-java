2d18c759b3249985c6d8e9a3b29019bd12249778
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-05 15:07:11 -0300

    Apenas testando

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,10 @@ public class Program {
         AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 
         System.out.println("Circle color: " + s1.getColor());
-        System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+        System.out.println("Circle area: " + String.format("%.3f", s1.area())); 
         System.out.println("Rectangle color: " + s2.getColor());
         System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+
+        System.out.println("Apenas um test");
     }
 }
